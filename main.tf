25163d852c0501caf0219e573e69599cd37b5fa5
ajaychava <ajay@achavacloud.com>
2024-07-28 18:51:23 -0500

    Applied TF fmt

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "aws_kms_key" "key" {
-  description             = var.description
-  deletion_window_in_days = var.deletion_window_in_days
-  enable_key_rotation     = var.enable_key_rotation
-  key_usage               = var.key_usage
+  description              = var.description
+  deletion_window_in_days  = var.deletion_window_in_days
+  enable_key_rotation      = var.enable_key_rotation
+  key_usage                = var.key_usage
   customer_master_key_spec = var.customer_master_key_spec
-  multi_region            = var.multi_region
-  policy = data.aws_iam_policy_document.kms_policy.json
-  tags = var.tags
+  multi_region             = var.multi_region
+  policy                   = data.aws_iam_policy_document.kms_policy.json
+  tags                     = var.tags
 }
 
 resource "aws_kms_alias" "this" {
